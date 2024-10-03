a9bcb9507523bb53c83a8f1d106de9363d24df76
Italiyaharikrushn <italiyaharikrushn4@gmail.com>
2024-10-03 18:29:25 +0530

    new change

--- todo/urls.py
+++ todo/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('todo/create/', views.create_todo, name="create_todo"),
     path('todo/delete/<int:todo_id>/', views.delete_todo, name="delete_todo"),
     path('todo/update/<int:todo_id>/', views.update_todo, name='update_todo'),
-    path('todo/complete/<int:todo_id>/', views.complete_todo, name='complete_todo'),
+    # path('todo/complete/<int:todo_id>/', views.complete_todo, name='complete_todo'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,28 +2,22 @@ from django.shortcuts import render, redirect
 from .models import Todo
 import datetime
 
-# Display the list of todos
 def todo_list(request):
     todos = Todo.objects.all()
     return render(request, 'todo/index.html', {'todos': todos})
 
-# Create a new todo
 def create_todo(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         if name:
             Todo.objects.create(name=name)
-            # messagebox.showinfo("Insert", "Inserted Successfully")
         return redirect('todo_list')
 
-# Delete a specific todo
 def delete_todo(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
     todo.delete()
     return redirect('todo_list')
     
-
-# Update a specific todo
 def update_todo(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
     if request.method == 'POST':
@@ -42,3 +36,4 @@ def complate_todo(request, todo_id):
         return redirect('todo_list')
     else:
         return False
+
